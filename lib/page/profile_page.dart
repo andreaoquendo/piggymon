36702a75a8e678a4039a887cb0a868546dc8818f
Andrea Oquendo <oquendo.3455@gmail.com>
2022-05-25 22:42:11 -0300

    UI do Perfil iniciado

--- lib/page/profile_page.dart
+++ lib/page/profile_page.dart
@@ -8,15 +8,113 @@ class ProfilePage extends StatelessWidget{
   Widget build(BuildContext context) => Scaffold(
     drawer: NavigationDrawerWidget(),
     appBar: AppBar(
-      title: Text('Perfil'),
+      title: const Text('Perfil'),
       centerTitle: true,
       backgroundColor: Colors.green,
     ),
-    /*body: const Center(
-        child: Text('Perfil', style: const TextStyle(fontWeight: FontWeight.bold), textScaleFactor: 3,)
-    ),*/
-    body: Text("Nome: \n\nEmail: \n\nData de Nascimento: \n\nGênero: ",
-          style: TextStyle(fontSize: 25)
-    ),
+    body: Container(
+        child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.lightGreen,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(15),
+                  bottomRight: Radius.circular(15)
+              ),
+            ),
+            padding: EdgeInsets.only(top:25, bottom:30),
+            alignment: Alignment.center,
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                const CircleAvatar(
+                    backgroundImage: NetworkImage('https://i.pinimg.com/236x/04/cb/51/04cb51d6983405b8e44a4eb67e7d90d7.jpg'),
+                    radius:50
+                ),
+                const SizedBox(height:15),
+
+                Text(
+                    "Bem vinda,",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35,
+                    )
+                ),
+                Text(
+                  "Anielle",
+                  style: TextStyle(
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 35,
+                )
+                )
+              ],
+            )
+          ),
+          SizedBox(height: 10),
+          _buildProfileItem("Nome:", "Anielle Olivera Silveira Passos"),
+          _buildProfileItem("Gênero:", "Feminino"),
+          _buildProfileItem("Aniversário:", "18/11/1998")
+        ],
+      ),
+    )
+    );
+}
+
+Widget _buildProfileItem(String item, String value){
+  return Container(
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal:16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.only(bottom: 5),
+                  child:Text(
+                      item,
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 15,
+                      )
+                  ),
+                ),
+                FractionallySizedBox(
+                  widthFactor: 1,
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        color: Color.fromRGBO(133, 133, 133, 0.3),
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    ),
+                    padding: EdgeInsets.only(
+                        bottom: 10,
+                        top:10,
+                        left: 15,
+                        right: 15
+                    ),
+                    child: Text(
+                        value,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18
+                        )
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )
   );
 }
